fd30971ef40f5427bc9407bab15f6b082ebddadc
ThomasVdbk <thomas.vdbk@gmail.com>
2023-12-20 14:51:28 +0100

    ajout isSpecialOffer /ShoppingList et creer /styles ShoppingList.css

--- my-app/src/components/ShoppingList.js
+++ my-app/src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -16,10 +17,13 @@ function ShoppingList() {
 			</ul>
 			<ul>
             {plantList.map((plant) => (
-                    <li key={ plant.id }>{plant.name}
+                    <li key={ plant.id } className='lmj-plant-item'>
+						{plant.name}
                         {plant.isBestSale ? <span>🔥</span> : null}
-                        {/* Fonctionne aussi avec cette condition */}
+						{/* Fonctionne aussi avec cette condition */}
                         {/* {plant.isBestSale && <span>🔥</span>} */}
+						{plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
+                        
                     </li>
             ))}
 			</ul>
