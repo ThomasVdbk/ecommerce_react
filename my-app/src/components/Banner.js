cb73b43d9b7aceadb53065428334d5d56d431028
ThomasVdbk <thomas.vdbk@gmail.com>
2023-12-20 11:40:37 +0100

    creation dossier styles + Banner.css et modif Banner.js + Cart.js.css

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -1,5 +1,28 @@
-function Banner() {
-    return <h1>La maison jungle</h1>
-}
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
 
+// Style avec className appelé par Banner.css.
+// L'attribut className permet de préciser une classe 
+// à un élément React pour lui indiquer du CSS.
+function Banner() {
+
+    return <div className='lmj-banner'>
+    <img src={logo} alt='La maison jungle' className='lmj-logo' />
+    <h1>La maison jungle</h1>
+</div>
+}
+// Autre possibilité de style avec inline style (non recommandé) 
+// function Banner() {
+//     return <div style={{
+//         color: 'black',
+//         textAlign: 'right',
+//         padding: 32, 
+//         borderBottom: 'solid 3px black'
+//     }}
+//     >
+// </div>
+// }
+
+// Autres possibilité avec 
+// https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/
 export default Banner
