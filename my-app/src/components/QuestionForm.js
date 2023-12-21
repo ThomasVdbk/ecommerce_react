d69562311bfb008a5e95b68360f81b84b0da9e05
ThomasVdbk <thomas.vdbk@gmail.com>
2023-12-21 16:53:06 +0100

    ajout message erreur mauvaise valeur input /QuestionForm

--- my-app/src/components/QuestionForm.js
+++ my-app/src/components/QuestionForm.js
@@ -4,15 +4,31 @@ import { useState } from 'react'
 // Function utilisant un formulaire contrôlé
 function QuestionForm() {
     const [inputValue, setInputValue] = useState('Posez votre question ici')
+    const [isInputError, setIsInputError] = useState(false);
+
+    // Fonction pour vérifier la valeur 'f'
+    const checkValue = (value) => {
+            if (!value.includes('f')) {
+            setInputValue(value);
+            setIsInputError(false); // Réinitialiser l'état d'erreur si la valeur est valide
+            } else {
+            setIsInputError(true);
+            }
+    };
 
     return (
         <div>
         <textarea
             value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+            onChange={(e) => checkValue(e.target.value)}
         />
         <button onClick={() => alert(inputValue)}>Alertez moi 🚨</button>
+
+        {isInputError && (
+            <div>🔥 Vous n'avez pas le droit d'utiliser la lettre "f" ici.</div>
+        )}
     </div>
+    
     )
 }
 
