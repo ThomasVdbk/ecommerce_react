f9008875cdf03db6e96905587597d100d6bd1def
ThomasVdbk <thomas.vdbk@gmail.com>
2023-12-26 19:43:12 +0100

    modif return affichage selon categorie

--- my-app/src/components/ShoppingList.js
+++ my-app/src/components/ShoppingList.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { plantList } from '../datas/plantList'
 import PlantItem from './PlantItem'
+import Categories from './Categories'
 import '../styles/ShoppingList.css'
 
 function ShoppingList({ cart, updateCart }) {
@@ -27,19 +28,28 @@ function ShoppingList({ cart, updateCart }) {
 	}
 
 	return (
-		<div>
-			<ul>
-				{categories.map((cat) => (
-					<li key={cat}>{cat}</li>
-				))}
-			</ul>
+		<div className='lmj-shopping-list'>
+			<Categories
+				categories={categories}
+				setActiveCategory={setActiveCategory}
+				activeCategory={activeCategory}
+			/>
+
 			<ul className='lmj-plant-list'>
-				{plantList.map(({ id, cover, name, water, light, price}) => (
-					<div key={id}>
-						<PlantItem cover={cover} name={name} water={water} light={light} />
-						<button onClick={() => addToCart(name, price)}>Ajouter</button>
-					</div>
-				))}
+				{plantList.map(({ id, cover, name, water, light, price, category }) =>
+					!activeCategory || activeCategory === category ? (
+						<div key={id}>
+							<PlantItem
+								cover={cover}
+								name={name}
+								water={water}
+								light={light}
+								price={price}
+							/>
+							<button onClick={() => addToCart(name, price)}>Ajouter</button>
+						</div>
+					) : null
+				)}
 			</ul>
 		</div>
 	)
