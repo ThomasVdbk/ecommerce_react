eb65de691d895705688f03952feed19ca825dcc6
ThomasVdbk <thomas.vdbk@gmail.com>
2023-12-26 18:40:39 +0100

    correction className

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -5,6 +5,7 @@ import ShoppingList from './ShoppingList'
 import Recommendation from './Recommendation'
 import QuestionForm from './QuestionForm'
 import Footer from './Footer'
+import '../styles/App.css'
 
 import { useState } from 'react'
 
@@ -20,7 +21,7 @@ function App() {
 			</Banner>
 			<Recommendation/>
 				
-			<div className='lmj-layout-inner'>
+			<div className='lmj-layout'>
                 <Cart cart={cart} updateCart={updateCart} />
                 <ShoppingList cart={cart} updateCart={updateCart} />
             </div>
